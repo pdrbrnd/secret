6c7213a902bc8799d270fd3de029d5d0616a013c
Pedro Brandão <pedro@significa.co>
2024-12-02 21:28:25 +0000

    add docs to shuffle

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -1,12 +1,20 @@
 // fisher-yates shuffle
 export function shuffle<T>(array: T[]): T[] {
+  // copy array to avoid mutating the original
   const arr = [...array];
 
-  for (let i = arr.length - 1; i > 0; i--) {
-    const index = Math.floor(Math.random() * (i + 1));
-    const temp = arr[i];
-    arr[i] = arr[index];
-    arr[index] = temp;
+  // start at the end
+  // we'll store the shuffled elements at the end of the array
+  for (let pointer = arr.length - 1; pointer > 0; pointer--) {
+    // get the current element at our pointer position
+    const current = arr[pointer];
+
+    // get a random index between 0 and the shuffled elements
+    const target = Math.floor(Math.random() * (pointer + 1));
+
+    // swap the current element with the target
+    arr[pointer] = arr[target]; // move the target to the current position (shuffled)
+    arr[target] = current; // move the current (unshuffled) to the position that was just emptied
   }
 
   return arr;
