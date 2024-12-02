82f644c53e3a8ef651b02c5f40fd5b151e1b388f
Pedro Brandão <pedro@significa.co>
2024-12-02 21:33:23 +0000

    refactor shuffle

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -3,18 +3,17 @@ export function shuffle<T>(array: T[]): T[] {
   // copy array to avoid mutating the original
   const arr = [...array];
 
-  // start at the end
-  // we'll store the shuffled elements at the end of the array
-  for (let pointer = arr.length - 1; pointer > 0; pointer--) {
-    // get the current element at our pointer position
-    const current = arr[pointer];
+  // start at the end and move backwards swapping the cursor element with a random element
+  let cursor = arr.length - 1;
 
-    // get a random index between 0 and the shuffled elements
-    const target = Math.floor(Math.random() * (pointer + 1));
+  while (cursor > 0) {
+    const target = Math.floor(Math.random() * (cursor + 1));
 
-    // swap the current element with the target
-    arr[pointer] = arr[target]; // move the target to the current position (shuffled)
-    arr[target] = current; // move the current (unshuffled) to the position that was just emptied
+    const temp = arr[cursor];
+    arr[cursor] = arr[target];
+    arr[target] = temp;
+
+    cursor--;
   }
 
   return arr;
